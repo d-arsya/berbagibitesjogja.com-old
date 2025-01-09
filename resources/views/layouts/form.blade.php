8b4976aa8da1de74985a8d8df81d76daba6e0edf
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-01-09 19:11:16 +0700

    [PERF] Add meta tag

--- resources/views/layouts/form.blade.php
+++ resources/views/layouts/form.blade.php
@@ -7,8 +7,27 @@
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
     <title>{{ $active ?? '' ? "$active |" : '' }}Berbagi Bites Jogja</title>
+    <meta name="description"
+        content="Berbagi Bites Jogja (BBJ) adalah Gerakan food rescue pertama di Jogja. Bergerak mengatasi Food Waste melalui Food Rescue dan Food Bank di wilayah Yogyakarta">
+
+    <meta property="og:url" content="https://heroes.berbagibitesjogja.site/">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Berbagi Bites Jogja">
+    <meta property="og:description"
+        content="Berbagi Bites Jogja (BBJ) adalah Gerakan food rescue pertama di Jogja. Bergerak mengatasi Food Waste melalui Food Rescue dan Food Bank di wilayah Yogyakarta">
+    <meta property="og:image"
+        content="https://berbagibitesjogja.site/wp-content/uploads/2024/09/1000049401-e1726334186949.jpg">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="heroes.berbagibitesjogja.site">
+    <meta property="twitter:url" content="https://heroes.berbagibitesjogja.site/">
+    <meta name="twitter:title" content="Berbagi Bites Jogja">
+    <meta name="twitter:description"
+        content="Berbagi Bites Jogja (BBJ) adalah Gerakan food rescue pertama di Jogja. Bergerak mengatasi Food Waste melalui Food Rescue dan Food Bank di wilayah Yogyakarta">
+    <meta name="twitter:image"
+        content="https://berbagibitesjogja.site/wp-content/uploads/2024/09/1000049401-e1726334186949.jpg">
 
 </head>
+
 <body>
     @include('components.header')
     <main class="md:px-44 px-6 py-10">
@@ -17,4 +36,5 @@
     </main>
     @include('components.footer')
 </body>
+
 </html>
