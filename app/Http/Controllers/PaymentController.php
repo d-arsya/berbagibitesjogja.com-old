f6258bb6560fdd310b11030471272633f2ac3513
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-02-13 09:38:33 +0700

    [FEAT] Deactivate wealth donor

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -84,6 +84,7 @@ class PaymentController extends Controller
     }
     public function create(Request $request)
     {
+        return redirect('https://berbagibitesjogja.site/beri-kontribusi');
         $ig_media = collect($this->getJsonData('https://graph.instagram.com/me/media?fields=media_url,permalink,media_type,thumbnail_url&access_token=' . env('INSTAGRAM_ACCESS_TOKEN', null))['data'])->take(9);
         $ig_user = collect($this->getJsonData('https://graph.instagram.com/me?fields=biography,followers_count,follows_count,media_count,name,profile_picture_url,username,website&access_token=' . env('INSTAGRAM_ACCESS_TOKEN', null)));
 
