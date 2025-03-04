e60fa82f3e0b7250536f0104407162216fcbcdb2
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-03-04 11:38:05 +0700

    [FEAT] Webhook for form submit

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\FormController;
 use App\Http\Controllers\Api\ReportController;
 use App\Http\Controllers\Api\UniversityController;
 use Illuminate\Http\Request;
@@ -10,3 +11,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 Route::get('university/{university}/faculty', [UniversityController::class, 'getFaculties']);
 Route::get('sponsor/{sponsor}/{start}/{end}', [ReportController::class, 'getDonations']);
+Route::post('form-submit', function () {
+    return abort(403);
+});
+Route::post('form-submit/{token}', [FormController::class, 'fromGoogleSheets']);
