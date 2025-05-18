e932881401830f3bb980ad239ff321ca12cbd99e
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-05-18 23:11:00 +0700

    test

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -78,42 +78,46 @@ class ReportController extends Controller
 
     public static function createReport(Donation $donation)
     {
-        $sponsor = $donation->sponsor;
-        $templateProcessor = new TemplateProcessor(public_path() . '/templates/default.docx');
-        $donationDate = \Carbon\Carbon::parse($donation->take)->isoFormat('dddd, D MMMM Y');
-        $templateProcessor->setValue('year', \Carbon\Carbon::now()->isoFormat('Y'));
-        $templateProcessor->setValue('createdDate', \Carbon\Carbon::now()->isoFormat('D-M-Y'));
-        $templateProcessor->setValue('sponsorName', $sponsor->name);
-        $templateProcessor->setValue('volunteerName', "Volunteer");
-        $templateProcessor->setValue('volunteerRole', "Volunteer");
-        $templateProcessor->setValue('donationDate', $donationDate);
-        $templateProcessor->setValue('foodTotal', round($donation->foods->sum('weight') / 100) / 10);
-        if ($donation->partner_id != null) {
-            $heroes = $donation->partner->heroes;
-        } else {
-            $heroes = $donation->heroes;
-        }
-        $templateProcessor->setValue('totalHeroes', $heroes->count());
-        $templateProcessor->cloneRow('heroesName', $heroes->count());
-        for ($i = 1; $i < $heroes->count() + 1; $i++) {
-            $templateProcessor->setValue("heroesName#$i", $heroes[$i - 1]->name);
-            if ($heroes[$i - 1]->quantity > 1) {
-                $templateProcessor->setValue("heroesFaculty#$i", $heroes[$i - 1]->quantity . " Orang");
+        try {
+            $sponsor = $donation->sponsor;
+            $templateProcessor = new TemplateProcessor(public_path() . '/templates/default.docx');
+            $donationDate = \Carbon\Carbon::parse($donation->take)->isoFormat('dddd, D MMMM Y');
+            $templateProcessor->setValue('year', \Carbon\Carbon::now()->isoFormat('Y'));
+            $templateProcessor->setValue('createdDate', \Carbon\Carbon::now()->isoFormat('D-M-Y'));
+            $templateProcessor->setValue('sponsorName', $sponsor->name);
+            $templateProcessor->setValue('volunteerName', "Volunteer");
+            $templateProcessor->setValue('volunteerRole', "Volunteer");
+            $templateProcessor->setValue('donationDate', $donationDate);
+            $templateProcessor->setValue('foodTotal', round($donation->foods->sum('weight') / 100) / 10);
+            if ($donation->partner_id != null) {
+                $heroes = $donation->partner->heroes;
             } else {
-                $templateProcessor->setValue("heroesFaculty#$i", $heroes[$i - 1]->faculty->name . " (" . $heroes[$i - 1]->faculty->university->name . ")");
+                $heroes = $donation->heroes;
             }
-        }
+            $templateProcessor->setValue('totalHeroes', $heroes->count());
+            $templateProcessor->cloneRow('heroesName', $heroes->count());
+            for ($i = 1; $i < $heroes->count() + 1; $i++) {
+                $templateProcessor->setValue("heroesName#$i", $heroes[$i - 1]->name);
+                if ($heroes[$i - 1]->quantity > 1) {
+                    $templateProcessor->setValue("heroesFaculty#$i", $heroes[$i - 1]->quantity . " Orang");
+                } else {
+                    $templateProcessor->setValue("heroesFaculty#$i", $heroes[$i - 1]->faculty->name . " (" . $heroes[$i - 1]->faculty->university->name . ")");
+                }
+            }
 
-        $foods = $donation->foods;
-        $templateProcessor->cloneRow('foodName', $foods->count());
-        for ($i = 1; $i < $foods->count() + 1; $i++) {
-            $templateProcessor->setValue("foodName#$i", $foods[$i - 1]->name);
-            $templateProcessor->setValue("foodWeight#$i", round($foods[$i - 1]->weight / 100) / 10);
+            $foods = $donation->foods;
+            $templateProcessor->cloneRow('foodName', $foods->count());
+            for ($i = 1; $i < $foods->count() + 1; $i++) {
+                $templateProcessor->setValue("foodName#$i", $foods[$i - 1]->name);
+                $templateProcessor->setValue("foodWeight#$i", round($foods[$i - 1]->weight / 100) / 10);
+            }
+            $filename = storage_path() . '/app/public/reports/' . 'Laporan ' . $sponsor->name . ' Tanggal ' . \Carbon\Carbon::parse($donation->take)->isoFormat('D MMMM Y');
+            $templateProcessor->saveAs($filename . '.docx');
+            $path = storage_path() . '/app/public/reports';
+            $files = File::allFiles($path);
+            return [$sponsor->name, $files[0]->getFilename()];
+        } catch (\Throwable $th) {
+            BotController::sendForPublic('120363399651067268@g.us', $th->getMessage(), 'SECOND');
         }
-        $filename = storage_path() . '/app/public/reports/' . 'Laporan ' . $sponsor->name . ' Tanggal ' . \Carbon\Carbon::parse($donation->take)->isoFormat('D MMMM Y');
-        $templateProcessor->saveAs($filename . '.docx');
-        $path = storage_path() . '/app/public/reports';
-        $files = File::allFiles($path);
-        return [$sponsor->name, $files[0]->getFilename()];
     }
 }
