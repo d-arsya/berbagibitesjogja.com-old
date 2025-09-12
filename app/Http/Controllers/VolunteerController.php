62e97e04ec5b22f6217ccb2aa63b66253ec0a713
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-09-12 12:04:42 +0700

    [STYLE] Hide some elements from volunteer

--- app/Http/Controllers/VolunteerController.php
+++ app/Http/Controllers/VolunteerController.php
@@ -47,33 +47,33 @@ class VolunteerController extends Controller
                 'foods' => $food_count,
             ];
         }
-        $donations = Donation::where('charity', 1)->whereBetween('take', [$fourMonthsAgo, $currentDate])->with(['foods', 'heroes'])->get();
-        $groupedData = $donations->groupBy(function ($donation) {
-            return Carbon::parse($donation->take)->format('Y-m');
-        });
-        $lastDataCharity = [];
-        foreach ($groupedData as $key => $item) {
-            $hero_count = 0;
-            $food_count = 0;
+        // $donations = Donation::where('charity', 1)->whereBetween('take', [$fourMonthsAgo, $currentDate])->with(['foods', 'heroes'])->get();
+        // $groupedData = $donations->groupBy(function ($donation) {
+        //     return Carbon::parse($donation->take)->format('Y-m');
+        // });
+        // $lastDataCharity = [];
+        // foreach ($groupedData as $key => $item) {
+        //     $hero_count = 0;
+        //     $food_count = 0;
 
-            foreach ($item as $data) {
-                $hero_count += $data->heroes->sum('quantity');
-                $food_count += $data->foods->sum('weight') / 1000;
-            }
-            $lastDataCharity[] = [
-                'bulan' => Carbon::parse($key)->format('F'),
-                'heroes' => $hero_count,
-                'foods' => $food_count,
-            ];
-        }
+        //     foreach ($item as $data) {
+        //         $hero_count += $data->heroes->sum('quantity');
+        //         $food_count += $data->foods->sum('weight') / 1000;
+        //     }
+        //     $lastDataCharity[] = [
+        //         'bulan' => Carbon::parse($key)->format('F'),
+        //         'heroes' => $hero_count,
+        //         'foods' => $food_count,
+        //     ];
+        // }
         $user = Auth::user();
         $donations = Donation::where('charity', 0)->get();
-        $donationsCharity = Donation::where('charity', 1)->get();
+        // $donationsCharity = Donation::where('charity', 1)->get();
         $precence = Precence::where('status', 'active')->count();
         $foods = Food::whereIn('donation_id', $donations->pluck('id'))->get();
-        $foodsCharity = Food::whereIn('donation_id', $donationsCharity->pluck('id'))->get();
+        // $foodsCharity = Food::whereIn('donation_id', $donationsCharity->pluck('id'))->get();
         $heroes = Hero::all();
-        return view('pages.volunteer.home', compact('user', 'donations', 'foods', 'donationsCharity', 'foodsCharity', 'heroes', 'lastData', 'lastDataCharity', 'precence'));
+        return view('pages.volunteer.home', compact('user', 'donations', 'foods', 'heroes', 'lastData', 'precence'));
     }
 
     public function index()
