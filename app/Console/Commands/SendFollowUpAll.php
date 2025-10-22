5468a988eac262c5e8bb7de24d4b839b286767b4
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-10-22 16:14:17 +0700

    [FEAT] Follow up to all

---
+++ app/Console/Commands/SendFollowUpAll.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\FormJob;
+use App\Traits\SendWhatsapp;
+use Illuminate\Console\Command;
+
+class SendFollowUpAll extends Command
+{
+    use SendWhatsapp;
+
+    protected $signature = 'follow-up:all';
+    public function handle()
+    {
+        $datas = FormJob::all()->pluck('data');
+        $data = collect($datas);
+        $tomorrow = now()->addDay()->toDateString();
+        $d = $data->firstWhere('date', $tomorrow);
+        if ($d) {
+            $output = '';
+            $output .= "📢 *Pemberitahuan Tugas Volunteer Besok*\n\n";
+            $output .= "📍 Donor: *{$d['sponsor']}*\n";
+            $output .= "🎯 Tujuan: *{$d['receiver']}*\n\n";
+            $output .= "Berikut kebutuhan tim dan jumlah orang yang dibutuhkan:\n";
+
+            foreach ($d['jobs'] as $job) {
+                $output .= "- {$job['name']} ({$job['place']}) — {$job['need']} orang\n";
+            }
+
+            $output .= "\n👥 *Daftar yang sudah bertugas:*\n";
+            foreach ($d['jobs'] as $job) {
+                $names = array_map(fn($p) => "{$p['name']} ({$p['phone']})", $job['persons']);
+                $namesStr = !empty($names) ? implode("\n", $names) : '-';
+                $output .= "\n🧩 {$job['name']}:\n{$namesStr}\n";
+            }
+
+            $output .= "\n⚠️ Bagi teman-teman volunteer yang ingin ikut berkontribusi di kegiatan ini, "
+                . "silakan isi jadwal tugas melalui link berikut ya:\n"
+                . "👉 war.berbagibitesjogja.com\n\n"
+                . "Terima kasih atas semangat dan partisipasinya 🌱";
+
+            $this->send('120363350581821641@g.us', $output);
+        }
+        return 0;
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('que:work --stop-when-empty')->timezone('Asia/Jakarta')->everyFiveSeconds();
+Schedule::command('follow-up:all')->timezone('Asia/Jakarta')->dailyAt('16.00')->name('Kirim notifikasi staff');
 Schedule::command('follow-up:staff')->timezone('Asia/Jakarta')->dailyAt('17.00')->name('Kirim notifikasi staff');
 Schedule::command('follow-up:volunteer')->timezone('Asia/Jakarta')->dailyAt('19.00')->name('Kirim notifikasi petugas');
 Schedule::command('clear:backup')->timezone('Asia/Jakarta')->dailyAt('20.00')->name('Hapus backup');
